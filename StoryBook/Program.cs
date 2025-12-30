effbb83fa686f2aed554c804b36f5f42d3f5fbe9
Peter.Chiu <tidub76217@hotmail.com>
2025-12-30 23:01:38 +0800

    fix: 修正 HSTS 預設值設定，確保 HTTPS 重導正確執行

--- StoryBook/Program.cs
+++ StoryBook/Program.cs
@@ -50,9 +50,9 @@ public class Program
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
